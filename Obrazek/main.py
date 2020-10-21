b69e4f6e793c70f0a07574bd4fa3c8674b6d1d4d
Jonyawatar <60350079+Jonyawatar@users.noreply.github.com>
2020-10-21 15:55:51 +0200

    nie wiem co się tu dzieje

--- Obrazek/main.py
+++ Obrazek/main.py
@@ -1,14 +1,38 @@
-image = { "type": "P1"
-          "comment": "komentarz",
-          "size": [2,3]
-          "pixels": [ [1,0],
-                      [0,1],
-                      [0,1],
-    }
-
 def load_image(obrazek):
-    return image
+    image = {"type":"",
+         "comment":"",
+         "size":[],
+         "pixels":[]}
+    
+    file=open(obrazek,"r+")
+    tak=file.read()
+    file_content=tak.split('\n')
+    print(file_content)
+    image["type"]=file_content.pop(0)
+    print(image)
+    print(file_content)
+    size=file_content.pop(0)
+    size=[int(size[0]),int(size[1])]
+    image["size"]
+    print(image)
+    print(file_content)
+
+    file.close()
+    return(image)
+    
+    
+def save_image(image,wombat):
+    file=open(wombat,"w+")
+    file.write(image["type"])
+    file.write(image["size"][0])
+    file.write(image["size"][1])
+    #file.write(image["pixels"]
+    file.close()
+    
 
 
-def save_image(obrazek, image):
-    pass
+
+ax=load_image("obrazek.pgm")
+save_image(ax,"adim.pgm")
+
+print(ax
