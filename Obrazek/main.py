68e0c8121a5e55957c8e16cfb49f4b15ddc365a1
Jonyawatar <60350079+Jonyawatar@users.noreply.github.com>
2020-11-04 16:24:15 +0100

    Update main.py
    już troche wiem co się dzieje

--- Obrazek/main.py
+++ Obrazek/main.py
@@ -7,32 +7,31 @@ def load_image(obrazek):
     file=open(obrazek,"r+")
     tak=file.read()
     file_content=tak.split('\n')
-    print(file_content)
     image["type"]=file_content.pop(0)
-    print(image)
-    print(file_content)
     size=file_content.pop(0)
+    size=size.split(' ')
     size=[int(size[0]),int(size[1])]
-    image["size"]
+    image["size"] = size
     print(image)
-    print(file_content)
-
     file.close()
     return(image)
     
     
 def save_image(image,wombat):
     file=open(wombat,"w+")
+    print(image)
     file.write(image["type"])
-    file.write(image["size"][0])
-    file.write(image["size"][1])
+    file.write(str(image["size"][0]))
+    file.write(str(image["size"][1]))
     #file.write(image["pixels"]
     file.close()
     
 
-
+ 
 
 ax=load_image("obrazek.pgm")
 save_image(ax,"adim.pgm")
 
-print(ax
+ 
+
+print(ax)
